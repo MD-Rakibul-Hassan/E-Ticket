34e8951cd731e3f718a8479710124cc060330f87
Rakibul <mdrakibulhassan2425@gmail.com>
2024-02-18 19:41:53 +0600

    full site with functionality added

--- scripts/main.js
+++ scripts/main.js
@@ -43,13 +43,22 @@ nextBtn.addEventListener("click", function (e) {
 const reloadPage = getId("relodBtn");
 reloadPage.addEventListener('click', function () {
     window.location.reload()
-})
+});
+// get copon input 
+const coponInpt = getId("coponInpt");
+
+// get apply button 
+const applyBtn = getId("applyBtn");
+// get Deiscounted price div 
+const discountedPrice = getId("discountedPrice");
+// get grand totalPrice 
+const grandTotal = getId("grandTotal");
+
 for (btn of ticketBtns) {
     //  addEventListener every single button 
     btn.addEventListener ("click",ticketClick);   
 }
 function ticketClick (e) {
-    
     // Change bg color clicking the button 
     if (seatCount < 4) {
         // get seat count element 
@@ -57,6 +66,7 @@ function ticketClick (e) {
         seatElem.innerText = seatCount;
         totalPrice += 550;
         totalPriceElement.innerText = totalPrice;
+        grandTotal.innerText = totalPrice;
         // seatLeft count 
         seatLeftCount -= 1;
         seatLeft.innerText = seatLeftCount;
@@ -74,7 +84,40 @@ function ticketClick (e) {
             if (e.target.value) {
                 nextBtn.removeAttribute("disabled");
             }
-        })
+        });
+        if (seatCount === 4) {
+            coponInpt.removeAttribute("disabled");
+            applyBtn.removeAttribute("disabled");
+        }
+        
+        
     }
     e.target.setAttribute("disabled", "true");  
+}
+applyBtn.addEventListener("click",function () {
+    let coponValue = coponInpt.value;
+    const splitedCoponValue = coponValue.split(" ").join("");
+    let coponValueUpperCase = splitedCoponValue.toUpperCase ();
+    if (coponValueUpperCase ===  "NEW15") {
+        getDiscount(totalPrice,15);
+        coponInpt.classList.add("hidden");
+        applyBtn.classList.add("hidden");
+    }else if (coponValueUpperCase === "COUPLE20") {
+        getDiscount(totalPrice,20);
+        coponInpt.classList.add("hidden");
+        applyBtn.classList.add("hidden");
+    } else {
+        alert("How have provide invalid copoun code !")
+    }
+});
+// function for get discount price 
+function getDiscount (totalPrice,discountAmount) {
+    const discount = (totalPrice * discountAmount) / 100;
+    discountedPrice.style.display = "flex";
+    discountedPrice.style.justifyContent = "space-between";
+    discountedPrice.style.margin = "20px"
+    discountedPrice.innerHTML = `<p>Discounted Price</p>
+    <p>${discount}</p>`;
+    const grandTotalPrice = totalPrice - discount;
+    grandTotal.innerText = grandTotalPrice;
 }
